97c77fad294f5abcc452842bc40c2186f0ed6913
mkl <mkl48@nau.edu>
2014-03-04 16:03:56 -0500

    update gitignor

--- old/src/modsMonitor.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-###Monitor r1
-wc -w ../results/modsr1.txt | mail -s 'hoth: mods monitor' mkl48@nau.edu
--- old/src/statsMonitor.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-###Monitor r1
-wc -w ../results/csr1.txt | mail -s 'hoth: stats monitor' mkl48@nau.edu
